f3474e7f91bb71e6953aab660cd9d8abde7e5d9c
Avdasov Oleksandr <agentfreedom@zoho.com>
2023-11-12 00:43:40 +0200

    solution for task 8

--- src/strings-tasks.js
+++ src/strings-tasks.js
@@ -133,8 +133,9 @@ function removeTrailingWhitespaces(value) {
  *   repeatString('', 3) => ''
  *   repeatString('abc', -2) => ''
  */
-function repeatString(/* str, times */) {
-  throw new Error('Not implemented');
+function repeatString(str, times) {
+  if (times < 0 || str === '') return '';
+  return str.repeat(times);
 }
 
 /**
